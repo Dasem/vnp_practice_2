acad5825aa99352b925391c2ae37e4d895ebb69f
dasem <gfhflbuvf46@gmail.com>
2020-12-04 20:06:09 +0500

    worked model

--- ssp.py
+++ ssp.py
@@ -13,13 +13,26 @@ from sklearn import tree
 def actualisation(train_test, start, end, creator):
     result_model = None  # Оверрайтнется энивей
     max_acc = 0
-    for i in range(start, end, 20):
-        (acc, mae, prediction, model) = create_model(train_test, i, creator)
-        if acc > max_acc:
-            result_model = model
-            max_acc = acc
-        print("Est =%d\t Acc = %f\t MAE = %f" % (i, acc, mae))
+    # dump_this("StochasticGradientDescent", train_test, lambda i: SGDClassifier(i['loss'], i['penalty'], i['max_iter']
+    # SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
+    # 'hinge', 'log', 'modified_huber',
+    #         'squared_hinge', 'perceptron', or a regression loss: 'squared_loss',
+    #         'huber', 'epsilon_insensitive', or 'squared_epsilon_insensitive'.
+
+    for loss in ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_loss', 'huber',
+                 'epsilon_insensitive', 'squared_epsilon_insensitive']:
+        for penalty in ['l2', 'l1', 'elasticnet']:
+            for max_iter in range(1, 100):
+                i = {'loss': loss, 'penalty': penalty, 'max_iter': max_iter}
+                (acc, mae, prediction, model) = create_model(train_test, i, creator)
+                if acc > max_acc:
+                    result_model = model
+                    result_mae = mae
+                    max_acc = acc
+                print("Acc = %f\t MAE = %f" % (acc, mae))
+    print("Result Acc = %f\t MAE = %f" % (max_acc, result_mae))
     return result_model
+
 
 def full_generation(model_name, train_test, creator):
     print("\n\n" + model_name + " first build")
@@ -42,14 +55,16 @@ def create_model(train_test, i, creator):
     mae = mean_absolute_error(train_test[3], prediction)
     return (sc, mae, prediction, model)
 
+
 def make_prediction(model, x, ids):
     return (model.predict(x))
+
 
 def dump_this(name, train_test, creator):
     np.random.seed(0)
     print("\n\nDump " + name + " this ...")
     model = full_generation(name, train_test, creator)
-    dump(model, 'trained/'+name+'.joblib')
+    dump(model, 'trained/' + name + '.joblib')
     if hasattr(model, 'n_estimators'):
         print(name + " selected model with NEst = %d" % (model.n_estimators))
     else:
@@ -58,7 +73,7 @@ def dump_this(name, train_test, creator):
 
 np.random.seed(0)
 
-training = pd.read_csv("train.csv")
+training = pd.read_csv("full_set.csv")
 
 # Create target and predictors variable
 y = training.result
@@ -69,16 +84,4 @@ print(predictors.columns)
 
 train_test = train_test_split(predictors, y, random_state=0)
 
-## MODELING
-dump_this("GradientBoostingClassifier", train_test, lambda i: GradientBoostingClassifier(n_estimators=i, learning_rate=.05))
-
-dump_this("RandomForest", train_test, lambda i: RandomForestClassifier(n_estimators=i))
-
-dump_this("SupportVectorClassification", train_test, lambda i: svm.SVC())
-
-dump_this("StochasticGradientDescent", train_test, lambda i: SGDClassifier())
-
-#custom
-dump_this("DecisionTreeClassifier", train_test, lambda i: tree.DecisionTreeClassifier())
-
-dump_this("ExtraTreesClassifier", train_test, lambda i: ExtraTreesClassifier(n_estimators=i))
+dump_this("StochasticGradientDescent", train_test, lambda i: SGDClassifier(i['loss'], i['penalty'], i['max_iter']))
